c75651b38c7f1bceb9ca05e648dffe4c53a866dc
samuel salcedo arango <165438782+Yuca-martines@users.noreply.github.com>
2026-09-16 22:59:20 -0500

    Update settings.py

--- Modelos_ML/RegresionLineal/front/config/settings.py
+++ Modelos_ML/RegresionLineal/front/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n5^^tc)!2mxl5ez1o8x*4$=(qwmm#tmg+3zwl_51juou9#3tvs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = [
+ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     "prediccionvivienda-a-f2bd.up.railway.app",
